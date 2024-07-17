54531b243d0fe5e982a0386a575dbf6763d4bad7
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-07-17 14:38:12 -0500

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/dbConfig";
+import shortUrl from "./routes/shortUrl";
+
+dotenv.config();
+connectDb();
+
+const port = process.env.PORT || 5001;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
+
+app.use("/api/", shortUrl);
+
+app.get("/", (req, res) => {
+  res.send("Api working");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
